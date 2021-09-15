3c80870d73bcb5ab0aa258f396b118cd58530948
ruben.verster <ruben.verster@1-grid.com>
2021-09-15 04:31:26 +0200

    remove pagination package

--- src/App.js
+++ src/App.js
@@ -4,17 +4,13 @@ import Article from './components/Article';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import axios from 'axios';
-import Pagination from 'react-js-pagination';
 
 require('dotenv').config();
 const { REACT_APP_ACCESS_KEY } = process.env;
 const BASE_URL = `https://newsapi.org/v2/everything?q=keyword&apiKey=${REACT_APP_ACCESS_KEY}`;
 
 const App = () => {
-  const [newsArticles, setNewsArticles] = useState(null);
-  const [activePage, setActivePage] = useState(1);
-
-  const handlePageChange = () => {};
+  const [newsArticles, setNewsArticles] = useState([]);
 
   const retrieveArticles = async () => {
     try {
